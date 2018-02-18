846490894e58db92716eda7f733087b0d796abaa
Andrew Campbell <andycampbell92@gmail.com>
2018-02-18 12:44:03 +0000

    tidy up git

--- fahrenheit_to_celsius/target/debug/fahrenheit_to_celsius.d
+++
@@ -1,1 +0,0 @@
-/Users/andy/git/rust-the-programming-language/fahrenheit_to_celsius/target/debug/fahrenheit_to_celsius: /Users/andy/git/rust-the-programming-language/fahrenheit_to_celsius/src/main.rs
