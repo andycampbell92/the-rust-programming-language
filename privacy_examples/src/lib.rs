51b965edd92b0c63d795d3f640d24e6cdb37766e
Andrew Campbell <andycampbell92@gmail.com>
2018-03-26 21:19:42 +0100

    basic privacy examples

--- privacy_examples/src/lib.rs
+++ privacy_examples/src/lib.rs
@@ -10,16 +10,18 @@ mod outermost {
 
     fn middle_secret_function() {}
 
-    mod inside {
-        pub fn inner_function() {}
+    pub mod inside {
+        pub fn inner_function() {
+            ::outermost::middle_secret_function()
+        }
 
-        fn secret_function() {}
+        pub fn secret_function() {}
     }
 }
 
 fn try_me() {
     outermost::middle_function();
-    outermost::middle_secret_function();
+    // outermost::middle_secret_function();
     outermost::inside::inner_function();
     outermost::inside::secret_function();
 }
